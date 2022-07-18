dbeaef2f5413dfd85af2bb6668f9a33d4c00b778
mustafaw1 <mustafazaidi840@gmail.com>
2022-07-18 22:54:06 +0500

    Refactor char array name

--- StringPrep/hashMapExample.java
+++ StringPrep/hashMapExample.java
@@ -3,13 +3,13 @@ public class hashMapExample {
     public static void main(String[] args) {
         String str = "aaaaabbbcccccef";
         HashMap<Character, Integer> count = new HashMap<Character, Integer>();
-        char[] Carr = str.toCharArray();
-        for (int i = 0; i < Carr.length; i++) {
-            if(count.containsKey(Carr[i])){
-                count.put(Carr[i], count.get(Carr[i]) + 1);
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if(count.containsKey(arr[i])){
+                count.put(arr[i], count.get(arr[i]) + 1);
             }
             else{
-                count.put(Carr[i], 1);
+                count.put(arr[i], 1);
             }
             
         }
