2f712f7cdbcfab90daefa08c06697bdda313d864
mustafaw1 <mustafazaidi840@gmail.com>
2022-10-05 20:19:06 +0500

    fix: Function name

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -1,10 +1,10 @@
-def twoSum(arr, target):
+def two_sum(arr, target):
     dict = {}
     n = 2
     result = [None] * n
     for i in range(len(arr)):
         diff = target - arr[i]
-        if (diff in dict):
+        if diff in dict:
             result[0] = diff
             result[1] = arr[i]
         dict[arr[i]] = arr[i]
@@ -14,4 +14,4 @@ def twoSum(arr, target):
 if __name__ == '__main__':
     arr = [2, 10, 9, 12, 8, 24]
     target = 12
-    print(twoSum(arr, target))
+    print(two_sum(arr, target))
