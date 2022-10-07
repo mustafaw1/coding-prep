078572b71ccd1e1c762dd150b889d89f0e0639c2
mustafaw1 <mustafazaidi840@gmail.com>
2022-10-08 00:13:47 +0500

    fix: improve variable name

--- Python/RomantoInt.py
+++ Python/RomantoInt.py
@@ -1,12 +1,12 @@
 def roman_2int(s):
-    map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result = 0
     for i in range(len(s) - 1):
-        if map[s[i]] > map[s[i + 1]]:
-            result += map[s[i]]
+        if roman_dict[s[i]] >roman_dict[s[i + 1]]:
+            result +=roman_dict[s[i]]
         else:
-            result -= map[s[i]]
-    result = result + map[s[len(s) - 1]]
+            result -=roman_dict[s[i]]
+    result = result +roman_dict[s[len(s) - 1]]
     return result
 
 
